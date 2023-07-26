697f8248109a27ce6a9e62e6c8fc447761f64a01
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-07-26 17:52:25 +0300

    fix: expense creation

--- src/constants/errors.constants.ts
+++ src/constants/errors.constants.ts
@@ -4,8 +4,6 @@ export const INVALID_AMOUNT_ERROR_MESSAGE = 'Amount must be a positive number';
 
 export const REQUIRED_AMOUNT_ERROR_MESSAGE = 'Amount is required';
 
-export const REQUIRED_CATEGORY_ERROR_MESSAGE = 'Category option is required';
-
 export const INVALID_EMAIL_ERROR_MESSAGE = 'Invalid email address';
 
 export const REQUIRED_EMAIL_ERROR_MESSAGE = 'Email is required';
--- src/constants/validation/add-expense.schema.ts
+++ src/constants/validation/add-expense.schema.ts
@@ -1,14 +1,7 @@
 import * as Yup from 'yup';
 
-import {
-  INVALID_AMOUNT_ERROR_MESSAGE,
-  REQUIRED_AMOUNT_ERROR_MESSAGE,
-  REQUIRED_CATEGORY_ERROR_MESSAGE,
-} from '@/constants/errors.constants';
+import { INVALID_AMOUNT_ERROR_MESSAGE, REQUIRED_AMOUNT_ERROR_MESSAGE } from '@/constants/errors.constants';
 
 export const addExpenseValidationSchema = Yup.object().shape({
   amount: Yup.number().positive(INVALID_AMOUNT_ERROR_MESSAGE).required(REQUIRED_AMOUNT_ERROR_MESSAGE),
-  categoryOption: Yup.string().required(REQUIRED_CATEGORY_ERROR_MESSAGE),
-  name: Yup.string(),
-  comment: Yup.string(),
 });
