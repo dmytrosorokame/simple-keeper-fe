3ad8295f0e0605202794817d3184c4f8437cbf55
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-07-29 17:23:15 +0300

    feat: add eye icon to password input

--- src/components/shared/Input/Input.tsx
+++ src/components/shared/Input/Input.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import cn from 'classnames';
-import React, { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import React, { DetailedHTMLProps, InputHTMLAttributes, useState } from 'react';
 
+import CloseEye from '@/components/icons/CloseEye';
 import Cross from '@/components/icons/Cross';
+import Eye from '@/components/icons/Eye';
 
 interface IInputProps {
   onCrossClick?: () => void;
@@ -13,27 +15,44 @@ interface IInputProps {
 
 type TInputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & IInputProps;
 
-const Input: React.FC<TInputProps> = ({ onCrossClick, isCrossVisible, error, ...rest }) => (
-  <div>
-    <div className="relative">
-      <input
-        {...rest}
-        className={cn(
-          'w-full h-10 pl-3 placeholder:text-black border-2 block outline-none appearance-none',
-          { ['border-black focus:border-black']: !error },
-          { ['border-red-400 focus:border-red']: error },
+const Input: React.FC<TInputProps> = ({ onCrossClick, isCrossVisible, error, type, ...rest }) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const isPasswordType = type === 'password';
+
+  const onEyeClick = (): void => {
+    setIsPasswordVisible((prev) => !prev);
+  };
+
+  return (
+    <div>
+      <div className="relative">
+        <input
+          {...rest}
+          type={isPasswordVisible ? 'text' : type}
+          className={cn(
+            'w-full h-10 pl-3 placeholder:text-black border-2 block outline-none appearance-none',
+            { ['border-black focus:border-black']: !error },
+            { ['border-red-400 focus:border-red']: error },
+          )}
+        />
+
+        {isCrossVisible && !isPasswordType && (
+          <button onClick={onCrossClick} className="absolute right-3 top-1/2 -translate-y-1/2">
+            <Cross />
+          </button>
         )}
-      />
 
-      {isCrossVisible && (
-        <button onClick={onCrossClick} className="absolute right-3 top-1/2 -translate-y-1/2">
-          <Cross />
-        </button>
-      )}
+        {isPasswordType && (
+          <button onClick={onEyeClick} className="absolute right-3 top-1/2 -translate-y-1/2">
+            {isPasswordVisible ? <CloseEye /> : <Eye />}
+          </button>
+        )}
+      </div>
+
+      {error && <p className="text-red-400 text-sm mt-1">{error}</p>}
     </div>
-
-    {error && <p className="text-red-400 text-sm mt-1">{error}</p>}
-  </div>
-);
+  );
+};
 
 export default Input;
