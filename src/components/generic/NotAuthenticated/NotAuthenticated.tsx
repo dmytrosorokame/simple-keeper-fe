cc80cd2efec8e929d37a29d3703452c25faa12c1
Дмитро Сорока <dmytro-soroka@MacBook-Air-Dmitro.local>
2023-08-12 08:19:47 +0300

    chore: dummy change

--- src/components/generic/NotAuthenticated/NotAuthenticated.tsx
+++ src/components/generic/NotAuthenticated/NotAuthenticated.tsx
@@ -7,6 +7,7 @@ const NotAuthenticated: React.FC = () => (
   <div className="text-center absolute w-screen h-screen top-0 left-0 bg-white flex justify-center items-center">
     <div>
       <h1 className="text-xl">Not authenticated 🔒</h1>
+
       <p>
         Please,{' '}
         <Link href={Pages.LOGIN} className="underline">
